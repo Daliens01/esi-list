840e8edc5d167c99a932092956b97e5721e9e92d
Daliens01 <ortizbarahona2@gmail.com>
2022-05-17 10:06:59 -0500

    configuracion cors next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
+module.exports = {
+  async rewrites() {
+      return [
+        {
+          source: '/api/task/:path*',
+          destination: 'https://esi-list.vercel.app/:path*',
+        },
+      ]
+    },
+};
 module.exports = nextConfig

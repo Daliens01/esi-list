c6efa76d4973e351bb70ed5f86242fe581834882
Daliens01 <ortizbarahona2@gmail.com>
2022-05-17 11:15:55 -0500

    cors v3

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@ import "./pages/api/task"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-const cors = {
+
   async rewrites() {
-      return [
-        {
-          source: '/api/task/:path*',
-          destination: 'https://esi-list.vercel.app/:path*',
-        },
-      ]
-    },
-};
-module.exports = nextConfig, cors
+    return [
+      {
+        source: '/api/task/',
+        destination: 'https://esi-list.vercel.app/',
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
